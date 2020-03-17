46dcefb1a02c575ca437ae0c4e3b1df1b03af865
RyutoEndo <endoh.pg@gmail.com>
2020-03-17 13:36:55 +0900

    既存タスクの表示

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,7 +17,11 @@ use App\Validator;
 */
 
 Route::get('/', function () {
-    return view('tasks');
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks',[
+    'tasks' => $tasks
+    ]);
 });
 
 /**
